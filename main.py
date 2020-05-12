c1c582dc43c77dbef79c133f39e1d81ae9b6efa7
goranaviani <goranaviani@gmail.com>
2020-05-12 16:44:43 +0200

    CSV-reader: Added a load_file fucntion that will cater of loading csv fies and returning the data

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import csv
 
-def main():
-    filename = "example.csv"
-
-
-    reader(filename)
-
-def reader(file_name):
+def load_file(file_name):
     with open(file_name) as file:
         file_data = [row for row in csv.DictReader(file)]
+    return file_data
 
+def main():
+    filename = "example.csv"
+    file_data = load_file(filename)
 
+    reader(file_data)
+
+def reader(file_data):
     header = file_data[0].keys()
     print(header)
     print(file_data)
